2a6bd1eac7d0111eccfa64de086559fd00c55b6f
mshige1979 <mshige1979@ryuouen.net>
2021-11-04 21:03:48 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <!-- navi area -->
+    <!--
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    -->
+
+    <!-- content area -->
+    <div class="contents">
+        <router-view />
+    </div>
   </div>
-  <router-view />
 </template>
 
 <style lang="scss">
---
+++ src/views/users/Login.vue
@@ -0,0 +1,24 @@
+/** Html */
+<template>
+  <div class="">
+    <div>
+        <div>UserID</div>
+        <input type="text" name="userid" class="" />
+    </div>
+    <div>
+        <div>Password</div>
+        <input type="password" name="password" class="" />
+    </div>
+    <div>
+        <button>aaaaaa</button>
+    </div>
+  </div>
+</template>
+
+/** Script */
+<script lang="ts">
+</script>
+
+/** Style */
+<style scoped lang="scss">
+</style>
